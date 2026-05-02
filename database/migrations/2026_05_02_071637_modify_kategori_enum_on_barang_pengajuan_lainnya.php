e49c693084c1d70ba837d99c8f745ea251cac110
gilngns <nsgilang62@gmail.com>
2026-05-02 14:17:05 +0700

    Refactor kategori enum in barang_pengajuan_lainnya migration for consistency in naming

---
+++ database/migrations/2026_05_02_071637_modify_kategori_enum_on_barang_pengajuan_lainnya.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        DB::statement("
+            ALTER TABLE barang_pengajuan_lainnya 
+            MODIFY kategori ENUM('habis pakai','tidak habis pakai')
+        ");
+    }
+
+    public function down(): void
+    {
+        DB::statement("
+            ALTER TABLE barang_pengajuan_lainnya 
+            MODIFY kategori ENUM('habis pakai','tidak_habis_pakai')
+        ");
+    }
+};
