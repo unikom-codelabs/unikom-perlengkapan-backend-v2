5c5250ab178d2e47967d0200d7d10a5fd7a88fe4
gilngns <nsgilang62@gmail.com>
2026-05-03 22:07:56 +0700

    Refactor UserSeeder to dynamically assign jabatan and unit IDs for user creation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,25 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
+use App\Models\Jabatan;
+use App\Models\UnitType;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        $jabatan = Jabatan::first();
+        $unit = UnitType::first();
 
         User::create([
             'nip' => 'ADM001',
             'username' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('password'),
+            'password' => bcrypt('password'),
             'jenis_kelamin' => 'Pria',
-            'jabatan_id' => 1,
-            'unit_id' => 1,
-            'role' => 'admin'
+            'jabatan_id' => $jabatan->id,
+            'unit_id' => $unit->id,
+            'role' => 'admin',
         ]);
+
+        $jabatan2 = Jabatan::skip(1)->first();
+        $unit2 = UnitType::skip(1)->first();
 
         User::create([
             'nip' => 'USR001',
@@ -28,9 +35,9 @@ class UserSeeder extends Seeder
             'email' => 'user@gmail.com',
             'password' => Hash::make('password'),
             'jenis_kelamin' => 'Wanita',
-            'jabatan_id' => 2,
-            'unit_id' => 2,
-            'role' => 'user'
+            'jabatan_id' => $jabatan2?->id ?? $jabatan->id,
+            'unit_id' => $unit2?->id ?? $unit->id,
+            'role' => 'user',
         ]);
     }
 }
