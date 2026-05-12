40260916367cf53550d67cc35bd465d0752985c0
gilngns <nsgilang62@gmail.com>
2026-05-12 15:09:27 +0700

    Update user name field in DaftarPengajuanResource to use username instead of nama

--- app/Http/Resources/DaftarPengajuanResource.php
+++ app/Http/Resources/DaftarPengajuanResource.php
@@ -14,7 +14,7 @@ class DaftarPengajuanResource extends JsonResource
 
             'user' => [
                 'id' => $this->user?->id,
-                'nama' => $this->user?->nama,
+                'nama' => $this->user?->username,
                 'nip' => $this->user?->nip,
                 'jabatan' => $this->user?->jabatan?->nama,
                 'unit' => $this->user?->unit?->nama,
