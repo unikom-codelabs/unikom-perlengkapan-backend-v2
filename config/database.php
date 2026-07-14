0973b6d0caa412c78a0d4f6f00fcc3352a88096b
gilngns <nsgilang62@gmail.com>
2026-07-14 19:46:35 +0700

    feat: menambahkan connections legacy di config

--- config/database.php
+++ config/database.php
@@ -114,6 +114,18 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'legacy' => [
+            'driver'    => 'mysql',
+            'host'      => env('LEGACY_DB_HOST', '127.0.0.1'),
+            'port'      => env('LEGACY_DB_PORT', '3306'),
+            'database'  => env('LEGACY_DB_DATABASE', 'perlengkapan_lama'),
+            'username'  => env('LEGACY_DB_USERNAME'),
+            'password'  => env('LEGACY_DB_PASSWORD'),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'strict'    => false,
+        ],
+
     ],
 
     /*
