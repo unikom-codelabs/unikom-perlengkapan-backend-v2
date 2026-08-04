5366d59cdaaf3d08b85bcf86ea3c0dc6cacd7f32
gilngns <nsgilang62@gmail.com>
2026-08-04 13:52:25 +0700

    fix: update AktivasiSeeder to create aktivasi for all pengajuan types

--- database/seeders/AktivasiSeeder.php
+++ database/seeders/AktivasiSeeder.php
@@ -9,12 +9,62 @@ class AktivasiSeeder extends Seeder
 {
     public function run(): void
     {
-        AktivasiPengajuan::create([
-            'id_pengajuan' => 1,
-            'aktif_mulai' => now()->subDays(7),
-            'aktif_selesai' => now()->addDays(7),
-            'tipe' => 'rutin',
-            'tahun_akademik' => '2025/2026'
-        ]);
+        $tahunAkademik = '2025/2026';
+        $mulai = now()->subDays(7);
+        $selesai = now()->addDays(90);
+
+        $pengajuanIds = \App\Models\Pengajuan::pluck('id', 'tipe');
+
+        // Aktivasi untuk kelas ganjil (id_pengajuan: kelas ganjil)
+        if ($pengajuanIds->has('kelas')) {
+            AktivasiPengajuan::firstOrCreate(
+                ['id_pengajuan' => $pengajuanIds['kelas'], 'tahun_akademik' => $tahunAkademik],
+                [
+                    'aktif_mulai' => $mulai,
+                    'aktif_selesai' => $selesai,
+                    'tipe' => 'rutin',
+                    'tahun_akademik' => $tahunAkademik,
+                ]
+            );
+        }
+
+        // Aktivasi untuk ujian (ambil yang pertama bertipe ujian)
+        if ($pengajuanIds->has('ujian')) {
+            AktivasiPengajuan::firstOrCreate(
+                ['id_pengajuan' => $pengajuanIds['ujian'], 'tahun_akademik' => $tahunAkademik],
+                [
+                    'aktif_mulai' => $mulai,
+                    'aktif_selesai' => $selesai,
+                    'tipe' => 'rutin',
+                    'tahun_akademik' => $tahunAkademik,
+                ]
+            );
+        }
+
+        // Aktivasi untuk tahunan
+        if ($pengajuanIds->has('tahunan')) {
+            AktivasiPengajuan::firstOrCreate(
+                ['id_pengajuan' => $pengajuanIds['tahunan'], 'tahun_akademik' => $tahunAkademik],
+                [
+                    'aktif_mulai' => $mulai,
+                    'aktif_selesai' => $selesai,
+                    'tipe' => 'rutin',
+                    'tahun_akademik' => $tahunAkademik,
+                ]
+            );
+        }
+
+        // Aktivasi untuk nonrutin
+        if ($pengajuanIds->has('nonrutin')) {
+            AktivasiPengajuan::firstOrCreate(
+                ['id_pengajuan' => $pengajuanIds['nonrutin'], 'tahun_akademik' => $tahunAkademik],
+                [
+                    'aktif_mulai' => $mulai,
+                    'aktif_selesai' => $selesai,
+                    'tipe' => 'nonrutin',
+                    'tahun_akademik' => $tahunAkademik,
+                ]
+            );
+        }
     }
 }
