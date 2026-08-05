10c5928db48c209198acd53b4cb06c582dfbe79c
RyanHandhika <muhammadryanhandhikasusantorya@gmail.com>
2026-08-05 11:05:29 +0700

    docs: update Swagger docs to all endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\SwaggerSpecController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/swagger', [SwaggerSpecController::class, 'ui']);
+Route::get('/swagger/spec', [SwaggerSpecController::class, 'spec']);
